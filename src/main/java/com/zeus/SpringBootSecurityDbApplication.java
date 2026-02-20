521704f826bb54d2cc44525ec4f090708ad83810
xsaz0826 <xsaz0826@gmail.com>
2026-02-20 17:27:19 +0900

    [생성] springboot security 설정 및 DB 연동

---
+++ src/main/java/com/zeus/SpringBootSecurityDbApplication.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeus/SpringBootSecurityDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
